ed3864d634a06144420f1191ae1b700b7939a47d
ritika-mittal03 <109079739+ritika-mittal03@users.noreply.github.com>
2022-07-11 19:03:17 +0530

    js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("name").innerHTML="Ritika Mittal";
+document.getElementById("roll-no").innerHTML="201500578";
+document.getElementById("university").innerHTML="GLA University, Mathura";
+
+var date=new Date();
+
+document.getElementById("date").innerHTML=date.toDateString(); 
